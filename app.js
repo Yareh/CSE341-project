7ad4135c4050be0358e353840551d1f2a6abec93
Yareh <minhah_chavez@hotmail.com>
2023-10-25 23:52:23 -0600

    cors changes

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const { ConectionDb } = require("./DB/conection");
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 app.use(express.json());
+const DIRECTORIO_PERMITIDO_CORS = "*";
+app.use(cors({
+    origin: DIRECTORIO_PERMITIDO_CORS
+}));
 
 ConectionDb((err) => {
     if (!err) {
